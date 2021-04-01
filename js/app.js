8d642066a6646fd339cd7c2e876639d75c24b8ee
eliasmg12345 <randyofgod666@gmail.com>
2021-04-01 19:43:02 -0400

    1

---
+++ js/app.js
@@ -0,0 +1,18 @@
+//Scope
+/*var musica ='rock';
+
+if(musica){
+    var musica='grunch';
+    console.log('dentro del if: ', musica);
+}
+console.log('fuera del if',musica);*/
+
+
+//Scope con leT
+let musica ='rock';
+
+if(musica){ 
+    let musica='grunchh';
+    console.log('dentro del if: ', musica);
+}
+console.log('fuera del if',musica);
